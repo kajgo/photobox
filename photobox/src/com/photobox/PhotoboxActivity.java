1e6816321850aba4488ddf432c5221b33002da23
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2012-05-03 17:23:38 +0200

    GUI input test.

--- photobox/src/com/photobox/PhotoboxActivity.java
+++ photobox/src/com/photobox/PhotoboxActivity.java
@@ -2,6 +2,10 @@ package com.photobox;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PhotoboxActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,15 @@ public class PhotoboxActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final TextView tv = (TextView) findViewById(R.id.textOutputField);
+        final EditText et = (EditText) findViewById(R.id.nameInputField);
+        final Button b = (Button) findViewById(R.id.nameInputButton);
+        b.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				tv.setText(et.getText());
+			}
+		});
     }
 }
