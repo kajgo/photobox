1ad06e49e4f27df9f62be495468714447d483f97
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2012-05-11 23:13:55 +0200

    Changed the matrix translation to use absolute coordinates.

--- photobox/src/com/photobox/OurCustomView.java
+++ photobox/src/com/photobox/OurCustomView.java
@@ -14,80 +14,81 @@ import android.view.View;
 
 public class OurCustomView extends View {
 
-	private float px = 20;
-	private float py = 20;
-	private float angle = 0;
-			
-	public OurCustomView(Context context) {
-		super(context);
-	}
-	
+    private float px = 20;
+    private float py = 20;
+    private float angle = 0;
+
+    public OurCustomView(Context context) {
+        super(context);
+    }
+
     public OurCustomView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
-    
-	public OurCustomView(Context context, AttributeSet attrs, int defStyle) {
+
+    public OurCustomView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
     }
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		Log.d("panic", "someone is touching me... help!!");
-		switch (event.getAction()) {
-		case MotionEvent.ACTION_DOWN:
-			Log.d("iiiih!", "don't push it...");
-			break;
-		case MotionEvent.ACTION_UP:
-			Log.d("phiew!", "back again!");
-			break;
-		case MotionEvent.ACTION_MOVE:
-			if (event.getPointerCount() > 1) {
-				float dh = event.getY() - py;
-				angle += dh;
-			} else {
-				px = event.getX();
-				py = event.getY();
-				Log.d("coords", "px=" + px + ", py=" + py);
-			}
-			invalidate();
-			break;
-		default:
-			break;
-		}
-		return true;
-	}
-	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		
-		Paint bgPaint = new Paint();
-		bgPaint.setARGB(255, 255, 255, 0);
-		canvas.drawPaint(bgPaint);
-		
-		Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.testimage_x);
-		
-		int border = 10;
-		int w = image.getWidth() + border * 2;
-		int h = image.getHeight() + border * 2;
-		
-		canvas.save();
-		
-		Matrix matrix = new Matrix();
-		matrix.preTranslate(px, py);
-		matrix.preRotate(angle);
-		canvas.setMatrix(matrix);
-		
-		Paint paint = new Paint();
-		paint.setARGB(255, 255, 255, 255);
-		canvas.drawRect(-w/2, -h/2, w/2, h/2, paint);
-		
-		canvas.drawBitmap(image, -w/2+border, -h/2+border, null);
-		
-		Paint p = new Paint();
-		p.setColor(Color.BLUE);
-		canvas.drawCircle(0, 0, 10, p);
-		
-		canvas.restore();
-	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d("panic", "someone is touching me... help!!");
+        switch (event.getAction()) {
+        case MotionEvent.ACTION_DOWN:
+            Log.d("iiiih!", "don't push it...");
+            break;
+        case MotionEvent.ACTION_UP:
+            Log.d("phiew!", "back again!");
+            break;
+        case MotionEvent.ACTION_MOVE:
+            if (event.getPointerCount() > 1) {
+                float dh = event.getY() - py;
+                angle += dh;
+            } else {
+                px = event.getRawX();
+                py = event.getRawY();
+            }
+            Log.d("coords", "px=" + px + ", py=" + py);
+            invalidate();
+            break;
+        default:
+            break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint bgPaint = new Paint();
+        bgPaint.setARGB(255, 255, 255, 0);
+        canvas.drawPaint(bgPaint);
+
+        Bitmap image = BitmapFactory.decodeResource(getResources(),
+                R.drawable.testimage_x);
+
+        int border = 10;
+        int w = image.getWidth() + border * 2;
+        int h = image.getHeight() + border * 2;
+
+        canvas.save();
+
+        Matrix matrix = new Matrix();
+        matrix.setTranslate(px, py);
+        matrix.preRotate(angle);
+        canvas.setMatrix(matrix);
+
+        Paint paint = new Paint();
+        paint.setARGB(255, 255, 255, 255);
+        canvas.drawRect(-w / 2, -h / 2, w / 2, h / 2, paint);
+
+        canvas.drawBitmap(image, -w / 2 + border, -h / 2 + border, null);
+
+        Paint p = new Paint();
+        p.setColor(Color.BLUE);
+        canvas.drawCircle(0, 0, 10, p);
+
+        canvas.restore();
+    }
 }
