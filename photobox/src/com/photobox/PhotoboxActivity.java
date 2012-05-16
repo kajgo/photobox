b653b963c070cdaf73a6a2539987445d12faec4b
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2012-05-16 13:28:42 +0200

    App in fullscreen.

--- photobox/src/com/photobox/PhotoboxActivity.java
+++ photobox/src/com/photobox/PhotoboxActivity.java
@@ -2,16 +2,17 @@ package com.photobox;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class PhotoboxActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.main);
     }
 }
