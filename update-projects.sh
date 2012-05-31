8804f77e229eae20c170a119148036caf9bc949e
Rickard Lindberg <ricli85@gmail.com>
2012-06-01 00:36:28 +0200

    Changed required sdk version to version 8.

--- update-projects.sh
+++ update-projects.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-android update project --name photobox --target android-10 --path photobox
+android update project --name photobox --target android-8 --path photobox
 android update test-project --main ../photobox --path photobox-test
