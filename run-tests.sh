da8c31f35cf9f52f270f96863fd431887115538b
Rickard Lindberg <ricli85@gmail.com>
2012-06-03 15:37:57 +0200

    Added check for test success message since exit code is broken.

--- run-tests.sh
+++ run-tests.sh
@@ -1,2 +1,17 @@
 #!/bin/sh
-(cd photobox-test && ant debug install test)
+LOGNAME=test.log
+(
+    cd photobox-test &&
+    ant debug install test 2>&1 | tee $LOGNAME &&
+    (
+        if grep '^     \[exec\] OK ([0-9]\+ tests)' $LOGNAME > /dev/null; then
+            rm $LOGNAME
+            exit 0
+        else
+            rm $LOGNAME
+            echo
+            echo "FAILURE: Didn't find test success part"
+            exit 1
+        fi
+    )
+)
