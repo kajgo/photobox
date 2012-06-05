65bc884ae53fbc23dae9295e314faa63e971da9f
Rickard Lindberg <ricli85@gmail.com>
2012-06-05 19:31:30 +0200

    Fixed style issues.

--- photobox/src/com/photobox/Renderer.java
+++ photobox/src/com/photobox/Renderer.java
@@ -9,18 +9,19 @@ public class Renderer {
     private GraphicalDebugger debugger;
     private WorldMapping mapping;
     private PhotoCollection collection;
-    
+
     public Renderer(GraphicalDebugger debugger, WorldMapping mapping, PhotoCollection collection) {
         this.debugger = debugger;
         this.mapping = mapping;
         this.collection = collection;
     }
+
     public void renderBackground(Canvas canvas) {
         Paint bgPaint = new Paint();
         bgPaint.setARGB(255, 255, 255, 0);
         canvas.drawPaint(bgPaint);
     }
-    
+
     public void renderPhoto(Canvas canvas, Photo photo) {
         Bitmap image = photo.image;
         float w = photo.width;
@@ -34,13 +35,13 @@ public class Renderer {
         paint.setARGB(255, 255, 255, 255);
         canvas.drawRect(-w / 2, -h / 2, w / 2, h / 2, paint);
 
-        canvas.drawBitmap(image, -w / 2 + photo.BORDER, -h / 2 + photo.BORDER,
-                null);
+        canvas.drawBitmap(image, -w / 2 + photo.BORDER, -h / 2 + photo.BORDER, null);
 
         debugger.drawCenterPoint(canvas);
         debugger.printAngle(canvas, photo);
         canvas.restore();
     }
+
     public void onDraw(Canvas canvas) {
         canvas.setMatrix(mapping.setFromWorld(canvas.getMatrix()));
         renderBackground(canvas);
