484c00bb6b504dc04c07401ffc6b43a88e95ec53
Rickard Lindberg <ricli85@gmail.com>
2012-06-05 20:18:49 +0200

    Compensate build scripts for buggy codemonitor.

--- super-test.sh
+++ super-test.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+# kill old
+
+pidfile=lastpid.txt
+
+if [ -e $pidfile ]; then
+    kill -9 $(cat $pidfile)
+    rm $pidfile
+fi
+
+echo $$ > $pidfile
+
 ./run-tests.sh &&
 (cd photobox && adb shell am start -n com.photobox/com.photobox.PhotoboxActivity)
+
+rm $pidfile
