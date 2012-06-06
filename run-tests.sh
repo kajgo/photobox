7e7104a643807587b3e6bbd0770526ace6053ad9
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2012-06-06 15:21:37 +0200

    Remove old class files in a good enough way.

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,37 @@
 #!/bin/sh
+
+remove_class_files_with_no_java_file() {
+    proj="$1"
+    classdir="$proj/bin/classes"
+    srcdir="$proj/src"
+    for classfile in $(find_class_files_in "$classdir"); do
+        java_file_name=$(echo "$classfile" | sed 's/\.class/\.java/')
+        java_file_full_path=$(join_paths "$srcdir" "$java_file_name")
+        if [ ! -e "$java_file_full_path" ]; then
+	    full_class_file=$(join_paths "$classdir" "$classfile")
+            echo "Removing: $full_class_file"
+            echo "       Not found: $java_file_full_path"
+            rm "$full_class_file"
+        fi
+    done
+}
+
+find_class_files_in() {
+    classdir="$1"
+    if [ -e "$classdir" ]; then
+        cd "$classdir"
+        find . -name '*.class'
+    fi
+}
+
+join_paths() {
+    echo "$1/$2" | sed 's%\./%%g'
+}
+
 LOGNAME="test_$$.log"
 (
-    (cd photobox && ant clean) &&
-    (cd photobox-test && ant clean) &&
+    remove_class_files_with_no_java_file photobox &&
+    remove_class_files_with_no_java_file photobox-test &&
     cd photobox-test &&
     ant debug install test 2>&1 | tee $LOGNAME &&
     (
