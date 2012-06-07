1e518814694b3e30149c99386b0675cb91e5027c
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2012-06-07 22:06:01 +0200

    Create import handler for photos.

--- photobox/src/com/photobox/OurCustomView.java
+++ photobox/src/com/photobox/OurCustomView.java
@@ -1,20 +1,17 @@
 package com.photobox;
 
-import java.util.ArrayList;
-
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.photobox.input.ImportHandler;
 import com.photobox.input.InputHandler;
 import com.photobox.renderer.Renderer;
 import com.photobox.renderer.WorldMapping;
 import com.photobox.world.GraphicalDebugger;
-import com.photobox.world.Photo;
 import com.photobox.world.PhotoCollection;
 
 public class OurCustomView extends View {
@@ -41,20 +38,9 @@ public class OurCustomView extends View {
         debugger = new GraphicalDebugger(mapping);
         renderer = new Renderer(debugger, mapping, collection);
 
-        BitmapFactory.Options options = new BitmapFactory.Options();
-        options.inSampleSize = 2;
-        ArrayList<Bitmap> imageList = new ArrayList<Bitmap>();
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.a, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.b, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.c, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.d, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.e, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.f, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.g, options));
-        imageList.add(BitmapFactory.decodeResource(getResources(), R.drawable.h, options));
-        for(int i = 0; i<imageList.size(); i++) {
-            collection.addPhoto(new Photo().withBitmap(imageList.get(i)));
-        }
+        ImportHandler importHandler = new ImportHandler();
+        Resources resources = getResources();
+        importHandler.importPhotos(collection, resources);
     }
 
     @Override
---
+++ photobox/src/com/photobox/input/ImportHandler.java
@@ -0,0 +1,32 @@
+package com.photobox.input;
+
+import java.util.ArrayList;
+
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import com.photobox.R;
+import com.photobox.world.Photo;
+import com.photobox.world.PhotoCollection;
+
+public class ImportHandler {
+
+    public void importPhotos(PhotoCollection collection, Resources resources) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 2;
+        ArrayList<Bitmap> imageList = new ArrayList<Bitmap>();
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.a, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.b, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.c, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.d, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.e, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.f, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.g, options));
+        imageList.add(BitmapFactory.decodeResource(resources, R.drawable.h, options));
+        for(int i = 0; i<imageList.size(); i++) {
+            collection.addPhoto(new Photo().withBitmap(imageList.get(i)));
+        }
+    }
+
+}
