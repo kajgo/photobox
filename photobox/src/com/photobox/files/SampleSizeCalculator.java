b265a3038dde75f2916f2d6b2a7677901af67b90
Kajsa Goffrich <kajsa.goffrich@gmail.com>
2012-07-10 16:58:59 +0200

    Play with sample size calculator.

--- photobox/src/com/photobox/files/SampleSizeCalculator.java
+++ photobox/src/com/photobox/files/SampleSizeCalculator.java
@@ -5,18 +5,28 @@ import com.photobox.files.BitmapSize;
 public class SampleSizeCalculator {
 
     public static int calculate(BitmapSize bitmapSize, BitmapSize reqSize) {
-        final int width = bitmapSize.width;
-        final int height = bitmapSize.height;
-        int inSampleSize = 1;
 
-        if (height > reqSize.height || width > reqSize.width) {
-            if (width > height) {
-                inSampleSize = Math.round((float)height / (float)reqSize.height);
-            } else {
-                inSampleSize = Math.round((float)width / (float)reqSize.width);
-            }
+        int boxSize = Math.max(reqSize.width, reqSize.height);
+
+        if (bitmapSize.width > bitmapSize.height) {
+            return Math.max(1, Math.round((float)bitmapSize.width/(float)boxSize));
+        } else {
+            return Math.max(1, Math.round((float)bitmapSize.height/(float)boxSize));
         }
-        return inSampleSize;
+
+
+//        final int width = bitmapSize.width;
+//        final int height = bitmapSize.height;
+//        int inSampleSize = 1;
+//
+//        if (height > reqSize.height || width > reqSize.width) {
+//            if (width > height) {
+//                inSampleSize = Math.round((float)height / (float)reqSize.height);
+//            } else {
+//                inSampleSize = Math.round((float)width / (float)reqSize.width);
+//            }
+//        }
+//        return inSampleSize;
     }
 
 }
