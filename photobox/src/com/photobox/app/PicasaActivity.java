df56b3b130c751c39cd325718f0c8fd8f244ae02
Rickard Lindberg and Kajsa Goffrich <ricli85+kajgo@gmail.com>
2012-10-14 16:08:54 +0200

    Improve picasa view more.

--- photobox/src/com/photobox/app/PicasaActivity.java
+++ photobox/src/com/photobox/app/PicasaActivity.java
@@ -14,19 +14,18 @@ import android.os.Environment;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.*;
 
 import com.photobox.R;
 
 public class PicasaActivity extends Activity {
 
-    private File currentPath;
-    private String[] currentItems;
-
-    private ListView list;
-    private TextView text;
+    private ListView albumList;
     private EditText usernameEditText;
+    private PicasaActivity thisActivity;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,14 +33,25 @@ public class PicasaActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        thisActivity = this;
+
         setContentView(R.layout.picasa);
 
         usernameEditText = (EditText)findViewById(R.id.usernameEditText);
+        albumList = (ListView)findViewById(R.id.albumList);
 
         Button loadAlbumsButton = (Button)findViewById(R.id.loadAlbumsButton);
         loadAlbumsButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Log.d("", "du heter " + usernameEditText.getText());
+                String foo = "" + usernameEditText.getText();
+                String[] items = {"one", "two", foo };
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                        thisActivity,
+                        android.R.layout.simple_list_item_1,
+                        android.R.id.text1,
+                        items);
+                albumList.setAdapter(adapter);
             }
         });
     }
